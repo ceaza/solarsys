b86fcace316be8bc251d7e04442de368bea7ccc7
Charles Allderman <charles@allderman.com>
2021-05-30 10:40:27 +0200

    live on pi

--- solarsys/core.py
+++ solarsys/core.py
@@ -1,14 +1,11 @@
 
 from pathlib import Path
 import logging
-import logging.config
 import configparser
 import time
 import os, sys
 import crcmod
 import json
-import os
-
 import random
 
 from multiprocessing import Process, Queue , Manager, Pool, Value , current_process
@@ -18,6 +15,7 @@ server = 'centurionsolar.co.za'
 emoncmspath ='emoncms11'
 
 config = configparser.ConfigParser()
+logconfig = configparser.ConfigParser()
 config['EMONCMS'] = {'apikey': '5501428544958e9fc4827004e36922e4',
                       'server': 'centurionsolar.co.za',
                       'emoncmspath': 'emoncms11'}
@@ -26,15 +24,11 @@ config['AXPERT'] = {'device': '/dev/hidraw0'
 config['NARADA'] = {'device': '/dev/ttyUSB0'
                         }
 
-config_file_name = Path(__file__).parent.parent.joinpath('conf.conf')
-logging_config_file_name = Path(__file__).parent.parent.joinpath('logging.conf')
+print(os.getcwd())
+config_file_name = Path('/home/pi/github/solarsys').joinpath('conf.conf')
 with open(config_file_name, 'w') as configfile:
     config.write(configfile)
-
 config.read(config_file_name)
-
-logging.config.fileConfig(logging_config_file_name)
-
 # create logger
 logger = logging.getLogger(__name__)
 
